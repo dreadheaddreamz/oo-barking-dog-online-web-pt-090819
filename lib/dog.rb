3fea9d78236752c90c3e7d50e242bcd887f36e2b
James Batsell <jamesbatsell05@gmail.com>
2019-09-24 16:09:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,12 @@ class Dog
   def bark
     puts "Woof!"
   end
-  def name(dog_name)
-    @this_dog_name = dog_name
+  def name = (dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+end
      
